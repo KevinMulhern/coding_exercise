b5c1a13e43dd29e3355f08d3566e45c958e088d5
KevinMulhern <kevinmulhern23@gmail.com>
2015-07-08 02:04:59 +0100

    Add sort algorithm and tests

--- lib/jobs_order.rb
+++ lib/jobs_order.rb
@@ -2,17 +2,48 @@ class JobsOrder
 
   attr_reader :sorted_jobs
   def initialize(jobs_string)
-    @job_hash = parse(jobs_string)
+    @jobs_hash = parse(jobs_string)
     @sorted_jobs = []
+    @checked = check_jobs
   end
 
   def parse(jobs_string)
-    job_hash = {}
+    jobs_hash = {}
     jobs_string.scan(/(\w) => ?(\w?)/).each do |job, dependency|
-    job_hash[job] = dependency
+    jobs_hash[job] = dependency
     end
-    return job_hash 
+    return jobs_hash 
   end
+
+  def check_jobs
+    checked = {}
+    @jobs_hash.each_key do |job|
+      checked[job] = "unchecked"
+    end
+    return checked
+  end
+
+  def sort
+    while @checked.values.include?("unchecked")
+      @checked.each_pair do |job, status|
+        visit(job) if status == "unchecked"
+      end
+    end
+  end
+
+  def visit(job)
+    if @checked[job] == "temp_check"
+      raise ArgumentError, "Jobs cannot have circular dependencies"
+    elsif @checked[job] == "unchecked"
+      @checked[job] = "temp_check"
+      visit(@jobs_hash[job]) unless @jobs_hash[job].empty?
+      @checked[job] = "checked"
+      @sorted_jobs << job
+    end   
+  end
+
 end
 
 p = JobsOrder.new("a =>")
+p.sort
+p p.sorted_jobs
--- spec/jobs_order_spec.rb
+++ spec/jobs_order_spec.rb
@@ -10,4 +10,85 @@ describe JobsOrder do
       jobs_order.parse(jobs_string).should be_eql({"a" => ""})
     end
   end
+
+  describe "sorting jobs" do
+    before(:each) do
+      jobs_order.sort
+    end
+    context "given an empty string" do
+      let(:jobs_string) {""}
+      it "should return []" do
+        expect(jobs_order.sorted_jobs).to eql([])    
+      end
+
+      it "should equal zero elements" do 
+        expect(jobs_order.sorted_jobs.size).to eql(0)
+      end
+    end
+
+    context "given one job" do
+      let(:jobs_string) {"a =>"}
+      it "should return one one job" do
+        expect(jobs_order.sorted_jobs).to eql(["a"])
+      end
+
+      it "should equal one elements" do 
+        expect(jobs_order.sorted_jobs.size).to eql(1)
+      end
+    end
+
+    context "given three jobs" do 
+      let(:jobs_string) {"a =>\nb =>\nc =>"}
+      it "should return abc job sequence" do
+        expect(jobs_order.sorted_jobs).to eql(["a", "b", "c"])
+      end
+
+      it "should equal three elements" do 
+        expect(jobs_order.sorted_jobs.size).to eql(3)
+      end
+    end
+
+    context "given three jobs and one dependency" do 
+      let(:jobs_string) {"a =>\nb => c\nc =>"}
+
+      it "should return job sequence acb" do
+        expect(jobs_order.sorted_jobs).to eql(["a", "c", "b"])
+      end
+
+      it "should be equal three elements" do 
+        expect(jobs_order.sorted_jobs.size).to eql(3)
+      end
+    end
+
+    context "given six jobs and four dependencies" do 
+      let(:jobs_string) {"a =>\nb => c\nc => f\nd => a\n e => b\n f =>"}
+
+      it "should return job sequence afcbde" do
+        expect(jobs_order.sorted_jobs).to eql(["a", "f", "c", "b", "d", "e"])
+      end
+
+      it "should be equal to 6 elements" do
+        expect(jobs_order.sorted_jobs.size).to eql(6)
+      end
+    end
+  end
+
+  describe "Error scenarios" do
+
+    context "given a job sequence with a self dependency" do
+      let(:jobs_order) {JobsOrder.new("a =>\nb =>\nc => c")}
+
+      it "should raise a self dependency error" do 
+        expect{ jobs_order.sort}.to raise_error(ArgumentError)
+      end
+    end
+
+    context "given a job sequence with a circular dependency" do 
+      let(:jobs_order) {JobsOrder.new("a =>\nb => c\nc => f\n d => a\ne =>\nf => b")}
+
+      it "should raise a circular dependency error" do
+        expect{ jobs_order.sort }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
